48cf86ae2d829d61d566ea40e41015f68ffff63f
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-08 14:27:28 +0400

    Преобразование типов

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-const projectName = 'cайт магазина';
-const price = 2000;
-const author = 'Василий Пупкин';
+const age = '18';
 
-const template = author + ' заказал ' + projectName + ' по цене ' + price + '$';
-console.log(template);
+console.log(Number(age) + 5);         // 23
 
-const template2 = `${author} заказал ${projectName} по цене ${price}$`;
-console.log(template2);
+console.log(age - 3);                 // 15
 
-const template3 = 'Проект \n' + 'Цена: ' + price + '$';
-console.log(template3);
-const template4 = `Проект
-Цена: ${price}$`;
-console.log(template4);
+console.log(age / 3);                 // 6
+
+console.log(age * 3);                 // 54
+
+const userName = 'Вася';
+
+console.log(Number(userName) + 5);    // NaN
+
+console.log(typeof NaN);              // number
+
+console.log(String(4) + 7);           // 47
+
+console.log(Boolean(0));              // fasle
+
+console.log(Boolean(1));              // true
+
+console.log(Boolean(123));            // true
+
+console.log(Boolean(-1));             // true
+
+console.log(Boolean('asdw'));         // true
+
+console.log(Boolean(''));             // false
+
+console.log(Boolean('') + 10);        // 10
+
+console.log(true + 2);                // 3
+
+const a = 2 + '10';
+console.log(a);                       // 210
+console.log(a - 10);                  // 200
