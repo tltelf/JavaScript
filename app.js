1647267d05863d01bb7bc0547066489f9686feee
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-08 12:37:17 +0400

    Revert "Порядок операторов"
    This reverts commit 30d75ee0d3d8c85714b46871e71e5d7a5f63babd.

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const isSuited = 100 - 10 > 90 - 5;
-console.log(isSuited);
+// Операторы присваивания
+let age = 18 + 6;
+age += 2; // age = age + 2
+age -= 3; // age = age - 2
+age *= 2; // age = age * 2
+age /= 2; // age = age / 2
 
-// 100 - (12 приоритет) 10 > (10 приоритет) 90 - (12 приоритет) 5
+age++; // age = age + 1
+age--; // age = age - 1
+console.log(age);
 
-const a = (6 + 10) / 2;
-console.log(a);
-
-let b;
-let c;
-c = b = 100 + 50 + 30;
-console.log(c);
-console.log(b);
+// Операторы сравнения
+const vasia = 20;
+console.log(age > vasia);
+console.log(age >= vasia);
+console.log(age < vasia);
+console.log(age <= vasia);
+console.log(age === vasia);
