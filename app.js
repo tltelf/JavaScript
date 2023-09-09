305eeab5745644f3f51862ec42db70d8e769b636
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-09 13:00:36 +0400

    Анонимные функции

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-function logName(name, surname) {
-  console.log(`Мое имя ${name} ${surname}`);
+function powerOfTwo(num) {
+  return num * num;
+}
+console.log(powerOfTwo(5));
+
+const poft = function (num) {
+  return num * num;
 }
 
-logName('Василий', 'Пупкин');
-
-function countDepositSum(depositInUSD, month, rate) {
-  return depositInUSD * (1 + rate / 12) ** month;
-}
-
-const example = countDepositSum(1000, 24, 0.12);
-console.log(example);
-
-console.log(countDepositSum(1000, 48, 0.11));
+console.log(poft(6));
