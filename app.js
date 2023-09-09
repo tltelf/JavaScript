fec044df6f6f2c608c12b96780702c662d9bd526
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-09 10:45:55 +0400

    Switch

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-const secretNumber = '7';
+const role = 'manager';
 
-if (secretNumber === 7) {
-  console.log('Угадал строго');
+if (role === 'manager') {
+  console.log('Менеджер')
+} else if (role === 'admin') {
+  console.log('Админ')
+} else if (role === 'ceo') {
+  console.log('СЕО')
+} else {
+  console.log('Мы тебя не знаем!')
 }
 
-if (secretNumber == 7) {
-  console.log('Угадал не строго');
+switch (role) {
+  case 'manager':  // role === 'manager
+    console.log('Менеджер');
+    break;
+  case 'admin':
+    console.log('Админ');
+    break;
+  case 'ceo':
+    console.log('СЕО');
+    break;
+  default:
+    console.log('Мы тебя не знаем');
+}
+switch (role) {
+  case 'manager':
+  case 'admin':
+    console.log('Не руководитель');
+    break;
+  case 'ceo':
+    console.log('Руководитель');
+    break;
+  default:
+    console.log('Мы тебя не знаем');
 }
 
-const q = Number(prompt('Введите число'));
-if (q === 7) {
-  console.log('!');
+const num = 1;
+
+switch (true) {
+  case num > 0:   // true === mum > 0
+    console.log('Положительный');
+    break;
+  case num < 0:
+    console.log('Отрицательный');
+    break;
+  default:
+    console.log('Ноль!');
 }
