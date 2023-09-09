d914de1444d810cd9efcc1841a4a6d6533932309
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-09 12:20:15 +0400

    Упражнение - Проверка прав

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-let userName = '';
-console.log(userName || 'Default Username');  // Default Username
-console.log(userName ?? 'Default Username');  // 
+/*
+	Пользователь хочет приобрести игру в магазине
+	Он может это сделать только если:
+	- Eго баланс больше 1000 (balance) 
+	или число бонусов больше 100 (bonusBalance)
+	- Он не забанен (isBanned)
+	- Игра не куплена (isExist)
+	- Игра в продаже (isSelling)
+	Напишите условие для покупки и выведите в консоль
+	результат
+*/
 
-let age = 0;
-console.log(age || 18);  // 18
-console.log(age ?? 18);  // 0
+const balance = 1100;
+const bonusBalance = 100;
+const isBanned = false;
+const isExist = false;
+const isSelling = true;
+
+const canBuy = (balance > 1000 || bonusBalance > 100)
+  && !isBanned
+  && !isExist
+  && isSelling;
+console.log(`Могу купить игру: ${canBuy ? 'да' : 'нет'}`);
