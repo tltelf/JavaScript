219c5eb2a086fcd5ff5f906b496cf98bfc5acd71
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-09 11:08:13 +0400

    Тернарные операторы

--- app.js
+++ app.js
@@ -1,49 +1,28 @@
-const role = 'manager';
+const bmwX3Price = 100000;
+const fordFocusPrice = 10000;
+const budget = 20000;
 
-if (role === 'manager') {
-  console.log('Менеджер')
-} else if (role === 'admin') {
-  console.log('Админ')
-} else if (role === 'ceo') {
-  console.log('СЕО')
+let message2 = budget > bmwX3Price 
+  ? 'BMW' 
+  : budget > fordFocusPrice 
+  ? 'Ford' : 'Велосипед';
+
+console.log(`Я хочу купить ${message2}`);
+
+let message;
+if (budget > bmwX3Price) {
+  message = 'BMW';
 } else {
-  console.log('Мы тебя не знаем!')
+  message = 'Велосипед';
 }
 
-switch (role) {
-  case 'manager':  // role === 'manager
-    console.log('Менеджер');
-    break;
-  case 'admin':
-    console.log('Админ');
-    break;
-  case 'ceo':
-    console.log('СЕО');
-    break;
-  default:
-    console.log('Мы тебя не знаем');
-}
-switch (role) {
-  case 'manager':
-  case 'admin':
-    console.log('Не руководитель');
-    break;
-  case 'ceo':
-    console.log('Руководитель');
-    break;
-  default:
-    console.log('Мы тебя не знаем');
-}
+console.log(`Я хочу купить ${budget > bmwX3Price ? 'BMW' : 'Велосипед'}`);
 
-const num = 1;
+const str = 10 > 0 ? 'Больше 0' : 'Не больше';
+console.log(str);
 
-switch (true) {
-  case num > 0:   // true === mum > 0
-    console.log('Положительный');
-    break;
-  case num < 0:
-    console.log('Отрицательный');
-    break;
-  default:
-    console.log('Ноль!');
+if (10 > 0) {
+  console.log('Больше 0');
+} else {
+  console.log('Не больше');
 }
