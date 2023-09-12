82948caff22969ada2a75f2818606f4aabe1e8a3
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-12 10:32:45 +0400

    Функции в функциях

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-function canAccessWebsite(age) {
-  if (age < 18) {
-    return 'Да';
-  }
-  return 'Нет';
+const KG_IN_USD = 7;
+const KM_IN_USD = 5;
+
+function calculateW(present) {
+  return present * KG_IN_USD;
 }
 
-console.log(canAccessWebsite(17));
+function calculateKm(distance) {
+  return distance * KM_IN_USD;
+}
 
-const canAccessWebsite2 = age => age < 18 ? 'Да' : 'Нет';
-const canAccessWebsite3 = age => age < 18;
+function getExchangePrice(present1, present2, distance) {
+  const price1 = present1 * KG_IN_USD;
+  const price2 = present2 * KG_IN_USD;
+  const distancePrice = distance * KM_IN_USD;
+  return price1 + price2 + distancePrice;
+}
 
-console.log(canAccessWebsite2(17));
-console.log(canAccessWebsite3(17));
+console.log(getExchangePrice(1, 2, 10));
+
+function getExchangePrice2(present1, present2, distance) {
+  const price1 = calculateW(present1);
+  const price2 = calculateW(present2);
+  const distancePrice = calculateKm(distance);
+  return price1 + price2 + distancePrice;
+}
+
+console.log(getExchangePrice2(1, 2, 10));
