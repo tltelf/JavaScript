c71c8609678fe752f0d06a594c15b67c1bd41ee1
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-13 17:49:12 +0400

    Rest оператор

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-const userData = ['Вася', 18, 'Тольятти'];
-
-function getData() {
-  return ['Вася', 18, 'Тольятти']
-}
-
-// const userName = getData()[0];
-// const userAge = userData[1];
-// const userCity = userData[2];
-
-const [userName, _, userCity] = userData;
-
-console.log(userName, userCity);          // Вася Тольятти
+const data = [1, 2, 3, 4, 5, 6];
+const [one, two, ...others] = data;
+console.log(one, two, others);
