f3a0fd4a84e75115fae1aebe62bd7fba52c87d90
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-13 22:22:54 +0400

    Упражнение - Функция разбора URL

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-const data = [1, 2, 3, 4, 5, 6];
-const [one, two, ...others] = data;
-console.log(one, two, others);
+/*
+	Дан произвольный url - 
+	'https://purpleschool.ru/course/javascript'
+	Нужно сделать функцию, которая выводит в консоль:
+	- Протокол (https)
+	- Доменное имя (purpleschool.ru)
+	- Путь внутри сайта (/course/javascript)
+*/
+
+const url = 'https://purpleschool.ru/course/javascript';
+
+function parseUrl(url) {
+  const [protocol, _, host, ...path] = url.split('/');
+  if (protocol === 'https:' || protocol === 'http:') {
+    if (!host.includes('.')) {
+      return;
+    }
+    console.log(protocol, _, host, path);
+    console.log(`Протокол: (${protocol.split(':')[0]})`);
+    console.log(`Доменное имя: (${host})`);
+    console.log(`Путь внутри сайта: (/${path.join('/')})`);
+  }
+}
+
+parseUrl(url);
