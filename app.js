34b02ca734c72d5ce18d3fb2779d091d5d4f00c7
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-15 22:10:04 +0400

    Filter

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const transactionInUSD = [10, -7, 50, -10, 100];
+const operations = [100, -20, 7, -10, 50];
 
-const transactionInRUB = [];
-for (const transaction of transactionInUSD) {
-  transactionInRUB.push(transaction * 60);
-}
+// const positiveOperations = [];
+// for (const operation of operations) {
+//   if (operation > 0) {
+//     positiveOperations.push(operation);
+//   }
+// }
+// console.log(positiveOperations);
 
-// console.log(transactionInUSD);       // [10, -7, 50, -10, 100]
-// console.log(transactionInRUB);       // [600, -420, 3000, -600, 6000]
+const positiveOperations = operations.filter(operation => operation > 0);
+const negativeOperations = operations.filter(operation => operation < 0);
+const positiveRUBOperations = operations
+  .filter(operation => operation > 0)
+  .map(operation => operation * 60);
 
-const transactionInRUB2 = transactionInUSD.map((transaction) => transaction * 60);
-
-console.log(transactionInUSD);
-console.log(transactionInRUB2);
+console.log(positiveOperations);      // [100, 7, 50]
+console.log(negativeOperations);      // [-20, -10]
+console.log(positiveRUBOperations);   // [6000, 420, 3000]
