b3a4d723a8263d19a47f57793092fbcdb4da5f24
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-16 02:27:45 +0400

    Flat и flatMap

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
-/*
-	Написать функцию, которые возващает true,
-	если элемент есть, и false, если нет.
-*/
-
-const arr = [2, 4, 4, 10, 20];
-
-// function some(arr, num) {
-//   for (el of arr) {
-//     if (el === num) {
-//       return el;
-//     }
-//   }
-// }
-
-function some(arr, num) {
-  return arr.find(el => el === num) === undefined ? false : true;
-}
-
-console.log(some(arr, 10));             // true
-
-console.log(arr.some(el => el === 1));  // false
+const prices = [[2, 4], [3, 4], [10, [20, 50]]];
+const res = prices.flat(10);
+const res2 = prices.flatMap(el => el.concat([1]));
+console.log(res);         // [2, 4, 3, 4, 10, 20, 50]
+console.log(res2);        // [2, 4, 1, 3, 4, 1, 10, [20, 50], 1]
