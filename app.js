71a54f860536c9c51b7fae7c845fa5edd5f9bfbd
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-16 00:01:18 +0400

    Упражнение - среднее значение

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
-const operations = [100, -20, 7, -30, 50];
+/* 
+	Найти среднее значение последовательности
+	чисел с помощью reduce
+*/
+const arr = [2, 4, 4, 10];
 
-let balance = 0;
-for (const operation of operations) {
-  balance += operation;
-}
-console.log(balance);
-
-const finalBalance = operations.reduce((acc, operation, i) => {
-  console.log(`Итерация ${i}, acc: ${acc}, operation: ${operation}`);
-  return acc += operation;
-}, 0);
-
-// 0 - acc = 0, operation = 100
-// 1 - acc = 100, operation = -20
-
-console.log(finalBalance);
-
-const minElement = operations.reduce((acc, operation) => {
-  if (operation > acc) {
-    return acc;
-  } else {
-    return operation;
+const avg = arr.reduce((acc, num, i, array) => {
+  if (i === array.length - 1) {
+    return (acc + num) / array.length;
   }
+  return acc + num;
 }, 0);
-console.log(minElement);
+
+console.log(avg);
