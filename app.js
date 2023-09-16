a119b435b126dccc6785bfd0e5179ea1a26e4c46
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-16 11:26:56 +0400

    Быстрое создание массивов

--- app.js
+++ app.js
@@ -1,34 +1,10 @@
-const users = ['Вася', 'Маша', 'Катя', 'Аня'];
-console.log(users);                               // ['Вася', 'Маша', 'Катя', 'Аня']
-users.sort();
-console.log(users);                               // ['Аня', 'Вася', 'Катя', 'Маша']
+const arr = [1, 2, 3, 4, 5];
 
-const operations = [100, -300, -100, 50, 480];
-console.log(operations);                          // [100, -300, -100, 50, 480]
-operations.sort();
-console.log(operations);                          // [-100, -300, 100, 480, 50]
+console.log(new Array(1, 2, 3, 4, 5));                          // [1, 2, 3, 4, 5]
+const arr2 = new Array(5);
+arr2.fill(1, 0, 3);
+arr2.fill(2, 3, 5);
+console.log(arr2);                                              // [1, 1, 1, 2, 2]
 
-// < 0 - a, b - сохраняем порядок
-// > 0 - b, a - меняем порядок
-const transactions = [100, -300, -100, 50, 480];
-// transactions.sort((a, b) => {
-//   if (a > b) {
-//     return 1;
-//   }
-//   if (a < b) {
-//     return -1;
-//   }
-// });
-transactions.sort((a, b) => a - b);
-console.log('transactions: ', transactions);      // [-300, -100, 50, 100, 480]
-
-const transactions2 = [100, -300, -100, 50, 480];
-transactions2.sort((a, b) => {
-  if (a < b) {
-    return 1;
-  }
-  if (a > b) {
-    return -1;
-  }
-});
-console.log('transactions2: ', transactions2);    // [480, 100, 50, -100, -300]
+const arr3 = Array.from({ length: 5 }, (cur, i) => i + 1);
+console.log(arr3);                                              // [1, 2, 3, 4, 5]
