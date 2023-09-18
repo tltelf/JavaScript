9a53cdc6892fdf44f14f1d108c4ca94a1db01f30
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-18 23:53:51 +0400

    Упражнение - выделение имени

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-const userName = 'Вася Пупкина';
-console.log(userName);                      // Вася Пупкина
-console.log(userName[0] + userName[1]);     // Ва
-console.log(userName.charAt(2));            // с
+/* вытащить имя и фамилию в отдельные перемнные */
+const fullUserName = 'Вася aka Terminator Perdinator Пупкин';
 
-console.log(userName.length);               // 12
-console.log(userName.indexOf('а'));         // 1
-console.log(userName.indexOf('уп'));        // 6
-console.log(userName.indexOf('упв'));       // -1
-console.log(userName.lastIndexOf('а'));     // 11
-console.log(userName.includes('уп'));       // true
-console.log(userName.includes('упd'));      // false
-
-console.log(userName.slice(5));             // Пупкина
-console.log(userName.slice(5, 8));          // Пуп
+const userName = fullUserName.slice(0, fullUserName.indexOf(' '));
+console.log(userName);
+const userSurname = fullUserName.slice(
+	fullUserName.lastIndexOf(' ') + 1,
+	fullUserName.length
+)
+console.log(userSurname);
