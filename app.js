fab614c73d319cf9176f53669cb4a2ecff263015
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-21 15:12:11 +0400

    Обращение к элементам

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const userArray = ['Вася', 'Пупкин', 24];
 const user = {
   name: 'Вася',
   surname: 'Пупкин',
@@ -7,5 +6,25 @@ const user = {
     'Программирование',
     'Готовка'
   ],
-  1: 1,
+  eduBasic: 'Школа 10',
+  eduPro: 'МФТИ'
 };
+
+console.log(user);
+console.log(user.name);             // Вася
+console.log(user.skills);           // [ 'Программирование', 'Готовка' ]
+console.log(user.skills[0]);        // Программирование
+
+console.log(user['skills']);        // [ 'Программирование', 'Готовка' ]
+
+const level = 'Pro';
+console.log(user['edu' + level]);   // МФТИ
+
+// const res = prompt('Введите свойство');
+// console.log(user[res]);
+user['city'] = 'Москва';
+console.log(user);
+
+user.age = 26;
+user['age'] = 26;
+console.log(user);
