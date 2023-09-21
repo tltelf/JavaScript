901cfa521dc14000c3eb8eafa9f9182f4793a97a
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-21 17:21:30 +0400

    Упражнение - кошелёк

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
-const user = {
-  name: 'Вася',
-  surname: 'Пупкин',
-  age: 24,
-  getFullName: function() {
-    return this.name + ' ' + this.surname;
-  }
-};
+/* Релизовать методы увеличения и уменьшения баланса, 
+	при котором каждая операция сохраняется в массив
+	operations в виде { reason: 'Оплата налогов', sum: -100 }.
+	Возвращается true, если успешно и false, если не хватает баланса
+	Так же реализовать метод вывода числа операций по кошельку
+ */
 
-console.log(user.getFullName());
+  const wallet = {
+    balance: 0,
+    operations: [],
+    increase: function(reason, sum) {
+      this.balance += sum;
+      this.operations.push({
+        reason: reason,
+        sum: sum,
+      })
+      console.log(`Пополнение баланса на сумму: ${sum}. Причина: "${reason}"`);
+      return true;
+    },
+    decrease: function(reason, sum) {
+      if (this.balance < sum) {
+        console.log(`Недостаточно средств. Ваш баланс: ${this.balance}. Сумма покупки: ${sum}`);
+        return false;
+      }
+      this.balance -= sum;
+      this.operations.push({
+        reason: reason,
+        sum: -sum,
+      })
+      console.log(`Покупка на сумму: ${sum} совершена успешно. Причина: "${reason}"`);
+      return true;
+    },
+    getOperations: function() {
+      console.log('Количество операций по балансу:');
+      return this.operations.length;
+    }
+  };
 
-function getFullName(user) {
-  return user.name + ' ' + user.surname;
-}
-
-const arr = [1, 2];
-arr.sort();
-'adsadasd'.toLowerCase();
+  console.log(wallet.increase('Заработал', 10000));
+  console.log(wallet.decrease('Заказал на МегаМаркете товары', 300));
+  console.log(wallet.decrease('Купил телефон', 30000));
+  console.log(wallet.decrease('Купил наушники', 5000));
+  console.log(wallet.getOperations());
