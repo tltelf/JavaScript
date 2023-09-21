e033c5a40b5f07711044759702d0326d47b4ebbd
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-21 17:39:48 +0400

    Итерирование по объекту

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const initialBalance = 7;
+const cities = {
+  msk: {
+    let: 200,
+    temp: 25,
+  },
+  spb: {
+    let: 100,
+    temp: 20,
+  }
+}
 
-const wallet = {
-	balance: initialBalance,
-	operations: [],
-};
+let sumTemp = 0;
+console.log(Object.keys(cities));
+let citiesCount = Object.keys(cities).length;
+// for (const key in cities) {
+//   sumTemp += cities[key].temp;
+// }
+// console.log(sumTemp / citiesCount);
 
-const balance = 10000;
-
-const wallet2 = {
-  // balance = balance равносильно balance
-  // Если название значения ключа совпадает с именем ключа, то можно опускать второе значение.
-	balance,
-	operations: [],
-};
-
-console.log(wallet2.balance);     // 10000
+for (const key of Object.keys(cities)) {
+  sumTemp += cities[key].temp;
+}
+console.log(sumTemp / citiesCount);
