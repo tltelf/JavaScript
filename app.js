c90ad8dfc43c1bbf6bd06c0a107a0ea4632905bb
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-25 00:55:53 +0400

    Arguments

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 'use strict';
+// let, var, const, func, arguments
+// Scope chain
+// this
 
-const user = {
-  firstName: 'Вася',
-  lastName: 'Пупкин',
-  age: 20,
-  getUserInfo: function() {
-    console.log(this);
-    console.log(`${this.firstName} ${this.lastName}`);
-
-    const canDrink = () => {
-      if (this.age >= 18) {
-        console.log('Может уже пить!');
-      } else {
-        console.log('Не может уже пить!');
-      }
-    }
-    canDrink();
-  },
-
-  getUserInfoArrow: () => {
-    console.log(this);
-    console.log(`${this.firstName} ${this.lastName}`);
-  }
+function sumNum(num1, num2) {
+  console.log(this);
+  console.log(arguments);
+  return num1 + num2;
 };
+console.log(sumNum(1, 4, 3, 7));
 
-user.getUserInfo();                     // Вася Пупкин
-user.getUserInfoArrow();                // undefined undefined
+const sumNumArr = (num1, num2) => {
+  console.log(this);
+  console.log(arguments);
+  return num1 + num2;
+};
+console.log(sumNumArr(1, 4, 3, 7));
