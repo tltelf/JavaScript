23f7f3a0b8aae72aebbef0c902f25cec12b8c0ba
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-25 13:06:55 +0400

    IIFE

--- app.js
+++ app.js
@@ -1,31 +1,14 @@
 'use strict';
-/*
-	Создайте объект пользователя с паролем.
-	С помощью функции ниже удалить пароль сделав
-	функцию сброса пароля
- */
+// IIFE
 
-function removePassword(reset) {
-	if (reset) {
-		this.password = undefined;
-	} else {
-		this.password = '1';
-	}
+function init() {
+  console.log('Start');
 };
+init();
 
-const user = {
-  password: 'pass12345'
-};
+(function() {
+  console.log('Start IIFO');
+  const a = 1;
+})();
 
-console.log(user);                                    // {password: 'pass12345'}
-removePassword.call(user, true);                      // Связываем с помощью call и удаляем пароль
-console.log(user);                                    // {password: undefined}
-user.password = 'NewPass';                            // Добавляем новый пароль
-console.log(user);                                    // {password: 'NewPass'}
-removePassword.apply(user, [true]);                   // Связываем с помощью apply и удаляем пароль
-console.log(user);                                    // {password: undefined}
-user.password = 'NEWPASS2';                           // Добавляем новый пароль
-console.log(user);                                    // {password: 'NEWPASS2'}
-const resetPassUser = removePassword.bind(user);      // Связываем с помощью bind
-resetPassUser(true);                                  // Удаляем пароль
-console.log(user);                                    // {password: undefined}
+console.log(a);
