554fcbed3d5aee74a8883fe64862d6c3b882d4f8
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-25 13:00:02 +0400

    Упражнение - управление this

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 'use strict';
+/*
+	Создайте объект пользователя с паролем.
+	С помощью функции ниже удалить пароль сделав
+	функцию сброса пароля
+ */
 
-const audi = {
-  make: 'Audi',
-  model: 'A3',
-  damages: []
+function removePassword(reset) {
+	if (reset) {
+		this.password = undefined;
+	} else {
+		this.password = '1';
+	}
 };
 
-const carManipulation = {
-  addDamage(part, rate) {
-    this.damages.push({ part, rate });
-    console.log(`Добавили повреждение на ${this.make} ${this.model}`);
-  }
-}
+const user = {
+  password: 'pass12345'
+};
 
-const addDamageAudi = carManipulation.addDamage.bind(audi);
-addDamageAudi('крыло', 3);
-console.log(audi);
-
-const addDamageAudiRoof = carManipulation.addDamage.bind(audi, 'крыша');
-addDamageAudiRoof(5);
-console.log(audi);
+console.log(user);                                    // {password: 'pass12345'}
+removePassword.call(user, true);                      // Связываем с помощью call и удаляем пароль
+console.log(user);                                    // {password: undefined}
+user.password = 'NewPass';                            // Добавляем новый пароль
+console.log(user);                                    // {password: 'NewPass'}
+removePassword.apply(user, [true]);                   // Связываем с помощью apply и удаляем пароль
+console.log(user);                                    // {password: undefined}
+user.password = 'NEWPASS2';                           // Добавляем новый пароль
+console.log(user);                                    // {password: 'NEWPASS2'}
+const resetPassUser = removePassword.bind(user);      // Связываем с помощью bind
+resetPassUser(true);                                  // Удаляем пароль
+console.log(user);                                    // {password: undefined}
