90775ff704caa8bac1aeb39641ca75101ee333a1
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-25 15:19:30 +0400

    Упражнение - работа с замыканиями

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
 'use strict';
 
-function changeBalance() {
-  let balance = 0;
-  let key = 'asd';  // Переменная не отобразится в scope, если её не использовать
-  return function(sum) {
-    balance += sum;
-    key = 'asdf';   // Здесь мы используем переменную и она будет отображена в scope (console.dir)
-    console.log(`Баланс: ${balance}`);
+/* 
+	Сделать функицю пользовтеля, которая берёт за основу
+	userInfo и за счёт замыкания создаёт новый объект, с
+	которым можно работать как user1().increse(100)
+*/
+
+const userInfo = {
+	balance: 0,
+	operations: 0,
+	increse(sum) {
+		this.balance +=sum;
+		this.operations++;
+	}
+}
+
+function getUser() {
+  const user = Object.assign({}, userInfo);
+  return function() {
+    return user;
   }
 };
 
-const change = changeBalance();
-change(100);
-change(-50);
-change(200);
-console.dir(change);
+// function getUser() {
+// 	const userObj = {
+// 		balance: 0,
+// 		operations: 0,
+// 		increse(sum) {
+// 			this.balance +=sum;
+// 			this.operations++;
+// 		}
+// 	};
+// 	return function() {
+// 		return userObj;
+// 	}
+// };
 
-const change2 = changeBalance();
-change2(100);
-console.dir(change2);
+const user1 = getUser();
+user1().increse(100);
+user1().increse(300);
+user1().increse(-50);
+console.log(user1());
+
+const user2 = getUser();
+user2().increse(100);
+console.log(user2());
