637e78d05c21c5ce0b79a9a9170c2236ac186c7f
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-28 14:40:54 +0400

    Local storage

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 'use strict';
 
-const panelText = 'Панель';
-const panelClass = 'button';
-const newElement = document.createElement('div');
-newElement.setAttribute('user-id', 1);
-newElement.classList.add('panel');
-// newElement.innerText = 'Кнопка';
-newElement.innerHTML = `<button class='${panelClass}'>${panelText}</button>`;
-document.querySelector('.test').appendChild(newElement);
+// setItem записывает значение в localStorage
+localStorage.setItem('token', 'sadasdas');
+localStorage.setItem('token1', 1);
+localStorage.setItem('token2', true);
+
+// getItem получает значение из localStorage
+console.log(localStorage.getItem('token'));             // sadasdas
+console.log(typeof localStorage.getItem('token'));      // string
+
+console.log(localStorage.getItem('token1'));            // 1
+console.log(typeof localStorage.getItem('token1'));     // string
+
+console.log(localStorage.getItem('token2'));            // true
+console.log(typeof localStorage.getItem('token2'));     // string
+
+// Удаляет конкретный item в localStorage
+localStorage.removeItem('token1');
+
+// Очищает localStorage
+localStorage.clear();
