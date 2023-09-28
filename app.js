ac54887a77786ae80e1c68f03fc3160dbc9050d7
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-28 11:11:57 +0400

    Работа со стилями и классами

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 'use strict';
 
-// document.querySelector('.button').addEventListener('click', function() {
-//   const input = document.querySelector('.input').value;
-//   if (!input) {
-//     return;
-//   }
-//   document.querySelector('.panel').innerText = input;
-//   document.querySelector('.input').value = '';
-// });
-
 function submitForm() {
   const input = document.querySelector('.input').value;
   if (!input) {
@@ -16,6 +7,9 @@ function submitForm() {
   }
   document.querySelector('.panel').innerText = input;
   document.querySelector('.input').value = '';
+  // document.querySelector('.notification').style.display = 'block';
+  // document.querySelector('.notification').classList.add('notification_active');
+  document.querySelector('.notification').classList.remove('notification_hidden');
 };
 
 function changeInput(e) {
@@ -23,9 +17,3 @@ function changeInput(e) {
     submitForm();
   }
 };
-
-// document.querySelector('.input').addEventListener('keydown', (e) => {
-//   if (e.code === 'Enter') {
-//     submitForm();
-//   }
-// });
