d54ec53820f5dfa3633a2b4568d1528f3ebc248d
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-28 14:54:17 +0400

    JSON

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
 'use strict';
 
-// setItem записывает значение в localStorage
-localStorage.setItem('token', 'sadasdas');
-localStorage.setItem('token1', 1);
-localStorage.setItem('token2', true);
-
-// getItem получает значение из localStorage
-console.log(localStorage.getItem('token'));             // sadasdas
-console.log(typeof localStorage.getItem('token'));      // string
-
-console.log(localStorage.getItem('token1'));            // 1
-console.log(typeof localStorage.getItem('token1'));     // string
-
-console.log(localStorage.getItem('token2'));            // true
-console.log(typeof localStorage.getItem('token2'));     // string
-
-// Удаляет конкретный item в localStorage
-localStorage.removeItem('token1');
-
-// Очищает localStorage
-localStorage.clear();
+const obj = JSON.parse('{ "a": 1 }');
+console.log(obj);                             // {a: 1}
+console.log(obj.a);                           // 1
+const str = JSON.stringify(obj);
+console.log(str);                             // '{"a":1}'
