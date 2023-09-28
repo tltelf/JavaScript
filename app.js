0a288156d4aae0bbf0684befa4ae99b2a1c1e172
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-28 13:59:39 +0400

    Добавление html на лету

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 'use strict';
 
-// Получение 1 элемента
-console.log(document.querySelector('.one').innerText);
-console.log(document.querySelector('span').innerText);
-console.log(document.querySelector('.one > span').innerText);
-console.log(document.querySelectorAll('.one')[0].innerText);
-console.log(document.getElementsByClassName('one')[0].innerText);
-
-// Получение 2 элемента
-console.log(document.querySelector('.one ~ div').innerText);
-console.log(document.querySelectorAll('.one')[1].innerText);
-console.log(document.getElementsByClassName('one')[1].innerText);
-
-// Получение 3 элемента
-console.log(document.querySelector('#two').innerText);
-console.log(document.getElementById('two').innerText);
-
-// Получение 4 элемента
-console.log(document.querySelector('[user-id="4"]').innerText);
+const panelText = 'Панель';
+const panelClass = 'button';
+const newElement = document.createElement('div');
+newElement.setAttribute('user-id', 1);
+newElement.classList.add('panel');
+// newElement.innerText = 'Кнопка';
+newElement.innerHTML = `<button class='${panelClass}'>${panelText}</button>`;
+document.querySelector('.test').appendChild(newElement);
