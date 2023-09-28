2abeb3160754aab993e867695209126a2d07f13b
Kirill Lyashenko <tltelf@yandex.ru>
2023-09-28 15:18:06 +0400

    Упражнение - сохранение JSON

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 'use strict';
 
-const obj = JSON.parse('{ "a": 1 }');
-console.log(obj);                             // {a: 1}
-console.log(obj.a);                           // 1
-const str = JSON.stringify(obj);
-console.log(str);                             // '{"a":1}'
+/* key 'text', value { text: 'sdfsdf' } */
+
+function submitForm() {
+  const input = document.querySelector('.input').value;
+  if (!input) {
+    return;
+  }
+  document.querySelector('.panel').innerText = input;
+  document.querySelector('.input').value = '';
+  document.querySelector('.notification').classList.remove('notification_hidden');
+  setLocalStorage(input);
+}
+
+function changeInput(e) {
+  if (e.code === 'Enter') {
+    submitForm();
+  }
+}
+
+function setLocalStorage(input) {
+  const obj = { text: input };
+  localStorage.setItem('text', JSON.stringify(obj));
+};
