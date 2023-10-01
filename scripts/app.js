8473ecec30e0651d14b1c4a0d4ec3435d8f0334b
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-01 22:50:30 +0400

    Загрузка и сохранение данных

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,23 @@
 'use strict';
 
-/* key 'text', value { text: 'sdfsdf' } */
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY'
 
-function submitForm() {
-  const input = document.querySelector('.input').value;
-  if (!input) {
-    return;
+/* utils */
+
+function loadData() {
+  const habbitString = localStorage.getItem(HABBIT_KEY);
+  const habbitArray = JSON.parse(habbitString);
+  if (Array.isArray(habbitArray)) {
+    habbits = habbitArray;
   }
-  document.querySelector('.panel').innerText = input;
-  document.querySelector('.input').value = '';
-  document.querySelector('.notification').classList.remove('notification_hidden');
-  setLocalStorage(input);
-}
-
-function changeInput(e) {
-  if (e.code === 'Enter') {
-    submitForm();
-  }
-}
-
-function setLocalStorage(input) {
-  const obj = { text: input };
-  localStorage.setItem('text', JSON.stringify(obj));
+  console.log(habbits);
 };
+
+function saveData() {
+  localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+};
+
+(() => {
+  loadData();
+})();
