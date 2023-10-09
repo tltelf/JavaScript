7bd675b80a5cff1c6cb861ed000becbd9d69328b
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-09 14:35:36 +0400

    WeakMap

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 'use strict';
-/* Необходимо поменять местами ключи и значения в следующем Map */
-const weatherMap = new Map([
-  ['London', 10],
-  ['Moscow', 7],
-  ['Paris', 14],
-]);
-console.log(weatherMap); // Map(3) {'London' => 10, 'Moscow' => 7, 'Paris' => 14}
 
-const reverseArrayWeatherMap = [...weatherMap].map((i) => i.reverse());
-const reverseWeatherMap = new Map(reverseArrayWeatherMap);
-console.log(reverseWeatherMap); // Map(3) {10 => 'London', 7 => 'Moscow', 14 => 'Paris'}
+let a = { a: 1 };
+let b = { b: 1 };
+const map = new WeakMap();
+map.set(a, 'testA');
+map.set(b, 'testB');
+console.log(map); // WeakMap {{…} => 'testA', {…} => 'testB'}
+console.log(map.get(a)); // testA
+console.log(map.has(a)); // true
+// console.log(map.delete(a));
+console.log(map); // WeakMap {{…} => 'testA', {…} => 'testB'}
 
-// Можно записать еще более коротко
-const reverseWeatherMap2 = new Map([...weatherMap].map((i) => i.reverse()));
-console.log(reverseWeatherMap2); // Map(3) {10 => 'London', 7 => 'Moscow', 14 => 'Paris'}
+a = null;
+setTimeout(() => {
+  console.log(map); // WeakMap {{…} => 'testB'}
+}, 1000);
+
+let cache = new WeakMap();
+
+function getValue(obj) {
+  if (!cache.has(obj)) {
+    const res = 1;
+    cache.set(obj, res);
+  }
+  return cache.get(obj);
+}
+
+const res = getValue(b);
+console.log(res); // 1
+
+const res2 = getValue(b);
+console.log(res2); // 1
