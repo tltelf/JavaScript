3ab0c6a1b07c26edae03edba435289500a4f1cca
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-09 13:13:46 +0400

    Итерация по Map

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 'use strict';
 
 const weatherMap = new Map([
-  ['London', '10'],
-  ['Moscow', '7'],
+  ['London', 10],
+  ['Moscow', 7],
+  ['Paris', 14],
 ]);
-console.log(weatherMap); // Map(2) {'London' => '10', 'Moscow' => '7'}
 
-const weatherObject = {
-  london: 10,
-  moscow: 7,
-  paris: 14,
-};
+// Итерация по Map
+for (const [key, value] of weatherMap) {
+  console.log(key); // London       // Moscow       // Paris
+  console.log(value); // 10           // 7            // 14
+}
 
-console.log(Object.entries(weatherObject)); // [['london', 10], ['moscow', 7], ['paris', 14]]
-const weatherMap2 = new Map(Object.entries(weatherObject));
-console.log(weatherMap2); // Map(3) {'london' => 10, 'moscow' => 7, 'paris' => 14}
-console.log(weatherMap2.size); // 3
+// Из Map в массив
+console.log([...weatherMap]); // [['london', 10], ['moscow', 7], ['paris', 14]];
+
+console.log(weatherMap.keys()); // MapIterator {'London', 'Moscow', 'Paris'}
+console.log(weatherMap.values()); // MapIterator {10, 7, 14}
+
+console.log([...weatherMap.keys()]); // ['London', 'Moscow', 'Paris']
+console.log([...weatherMap.values()]); // [10, 7, 14]
