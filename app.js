79b15e843161c91314e9e4df7bb2e741f4364c80
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-10 17:09:05 +0400

    Округление чисел

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 'use strict';
 
-console.log(Math.sqrt(36)); // 6
-console.log(36 ** (1 / 2)); // 6
-console.log(Math.cbrt(27)); // 3
+// Math.round округляет до ближайшего целого числа
+console.log(Math.round(1.4)); // 1
+console.log(Math.round(1.6)); // 2
+console.log(Math.round(1.5)); // 2
+console.log(Math.round(1.4999)); // 1
 
-console.log(16 ** (1 / 4)); // 2
-console.log(Math.pow(16, 1 / 4)); // 2
+// Math.ceil округляет до верхнего значения
+console.log(Math.ceil(1.1)); // 2
+console.log(Math.ceil(1)); // 1
+console.log(Math.ceil(0.1)); // 1
 
-console.log(Math.sign(-100)); // -1
-console.log(Math.sign(35)); // 1
+// Math.floor округляет до нижнего значения
+console.log(Math.floor(2.1)); // 2
+console.log(Math.floor(2.9)); // 2
 
-console.log(Math.abs(-100)); // 100
-console.log(Math.abs(100)); // 100
+// Math.trunc обрезает до целого
+console.log(Math.trunc(2.9)); // 2
+console.log(Math.trunc(2.1)); // 2
+console.log(Math.trunc(1.11111)); // 1
 
-console.log(Math.exp(3)); // 20.085536923187668
-
-console.log(Math.max(1, -2, 10, 0, 19)); // 19
-console.log(Math.max(1, -2, '10', true, '19')); // 19
-console.log(Math.max(1, -2, '10', true, '19s')); // NaN
-
-console.log(Math.min(1, -2, '10', true, '19')); // -2
-
-const arr = [1, -2, 10, 0, 19];
-console.log(Math.max(arr)); // NaN
-// С помощью spread оператора наш массив преобразовался в аргументы функции max()
-console.log(Math.max(...arr)); // 19
-
-console.log(Math.random());
+// toFixed() округление до определенного числа знаков после запятой и возвращение строки
+console.log((1.4999).toFixed(1)); // '1.5'
+// Чтобы вернуть строку, можно преобразовать с помощью Number()
+console.log(Number((1.4999).toFixed(1))); // 1.5
