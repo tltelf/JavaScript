fc7fbb2488ded01329023d1ca0e21c6bc012a884
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-13 16:06:16 +0400

    Интернационализация дат

--- app.js
+++ app.js
@@ -1,58 +1,24 @@
 'use strict';
 
-/*
-	Сделать функцию, которая принимает пользователя и
-	проверяет, есть ли у него сегодня день рождения или нет
-*/
+const date = new Date();
 
-// Создание объектов пользователей с их именем и датой рождения
-const user = {
-  name: 'Vasia',
-  birthday: '10/13/2023',
+console.log(date); // Fri Oct 13 2023 16:04:50 GMT+0400 (Самарское стандартное время)
+console.log(new Intl.DateTimeFormat('ru-RU').format(date)); // 13.10.2023
+const options1 = {
+  hour: 'numeric',
+  minute: 'numeric',
 };
+console.log(new Intl.DateTimeFormat('ru-RU', options1).format(date)); // 16:04
 
-const user2 = {
-  name: 'Petya',
-  birthday: '10/16/2023',
+const options2 = {
+  hour: 'numeric',
+  minute: 'numeric',
+  month: 'long',
+  weekday: 'short',
+  year: '2-digit',
 };
+console.log(new Intl.DateTimeFormat('en-US', options2).format(date)); // October 23 Fri at 4:04 PM
+console.log(new Intl.DateTimeFormat('ru-RU', options2).format(date)); // октябрь 23 г. пт в 16:04
 
-// Первая функция, которая проверяет день рождения пользователя
-function isBirthday(user) {
-  // Получение текущей даты и времени
-  const today = new Date();
-  // Создание новой даты без времени (только год, месяц и день)
-  const todayWithoutTime = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate()
-  );
-  // Преобразование даты дня рождения пользователя в объект Date
-  const birthdayDate = new Date(user.birthday);
-  // Сравнение текущей даты с днем рождения пользователя
-  // Преобразуем обе даты в числа и сравниваем их
-  return Number(todayWithoutTime) === Number(birthdayDate);
-}
-
-// Проверка первой функции для двух пользователей и вывод результата
-console.log(isBirthday(user)); // true
-console.log(isBirthday(user2)); // false
-
-// Вторая функция, которая также проверяет день рождения пользователя
-function isBirthday2(user) {
-  // Получение текущей даты и времени
-  const now = new Date();
-  // Преобразование даты дня рождения пользователя в объект Date
-  const userBirthdayDate = new Date(user.birthday);
-  // Проверка, совпадает ли месяц и день дня рождения пользователя
-  if (now.getMonth() !== userBirthdayDate.getMonth()) {
-    return false;
-  }
-  if (now.getDate() !== userBirthdayDate.getDate()) {
-    return false;
-  }
-  return true;
-}
-
-// Проверка второй функции для двух пользователей и вывод результата
-console.log(isBirthday2(user)); // true
-console.log(isBirthday2(user2)); // false
+console.log(navigator.language); // ru-RU
+console.log(new Intl.DateTimeFormat(navigator.language, options1).format(date)); // 16:04
