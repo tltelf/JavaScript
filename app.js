fee505e12bbd187fb26c1f3bbcb3430b54a88778
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-13 15:52:17 +0400

    Упражнение - Проверка на день рождения

--- app.js
+++ app.js
@@ -1,20 +1,58 @@
 'use strict';
 
-const first = new Date(2024, 10, 4);
-const second = new Date(2024, 10, 3);
+/*
+	Сделать функцию, которая принимает пользователя и
+	проверяет, есть ли у него сегодня день рождения или нет
+*/
 
-const third = new Date(2024, 10, 10);
-const fourth = new Date(2024, 10, 10);
+// Создание объектов пользователей с их именем и датой рождения
+const user = {
+  name: 'Vasia',
+  birthday: '10/13/2023',
+};
 
-console.log(first.getTime()); // 1730664000000
-console.log(first.getTime() > second.getTime()); // true
-console.log(first > second); // true
-console.log(first < second); // false
+const user2 = {
+  name: 'Petya',
+  birthday: '10/16/2023',
+};
 
-console.log(third == fourth); // false потому что сравниваются ссылки на объекты, а не сами объекты
-console.log(third === fourth); // false
+// Первая функция, которая проверяет день рождения пользователя
+function isBirthday(user) {
+  // Получение текущей даты и времени
+  const today = new Date();
+  // Создание новой даты без времени (только год, месяц и день)
+  const todayWithoutTime = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate()
+  );
+  // Преобразование даты дня рождения пользователя в объект Date
+  const birthdayDate = new Date(user.birthday);
+  // Сравнение текущей даты с днем рождения пользователя
+  // Преобразуем обе даты в числа и сравниваем их
+  return Number(todayWithoutTime) === Number(birthdayDate);
+}
 
-console.log(third.getTime() == fourth.getTime()); // true
-console.log(third.getTime() === fourth.getTime()); // true
-console.log(Number(third) === Number(fourth)); // true
-console.log(+third === +fourth); // true
+// Проверка первой функции для двух пользователей и вывод результата
+console.log(isBirthday(user)); // true
+console.log(isBirthday(user2)); // false
+
+// Вторая функция, которая также проверяет день рождения пользователя
+function isBirthday2(user) {
+  // Получение текущей даты и времени
+  const now = new Date();
+  // Преобразование даты дня рождения пользователя в объект Date
+  const userBirthdayDate = new Date(user.birthday);
+  // Проверка, совпадает ли месяц и день дня рождения пользователя
+  if (now.getMonth() !== userBirthdayDate.getMonth()) {
+    return false;
+  }
+  if (now.getDate() !== userBirthdayDate.getDate()) {
+    return false;
+  }
+  return true;
+}
+
+// Проверка второй функции для двух пользователей и вывод результата
+console.log(isBirthday2(user)); // true
+console.log(isBirthday2(user2)); // false
