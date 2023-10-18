fcdf2290479c54a8511b359b3f3b9581e1cba991
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-18 20:48:48 +0400

    Приватные методы и свойства

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 'use strict';
 
-class Test {
-  static a = 1;
-  static hello() {
-    console.log('Hello');
+class Car {
+  #vin; // Приватное свойство #vin
+
+  speed; // Обычное свойство speed
+
+  constructor() {
+    this.#test2 = 5; // Приватное свойство #test2 (примечание: имя #test2 не определено выше)
+    this.test3 = 5; // Обычное свойство test3
   }
 
+  #changeVin() {
+    console.log('changed');
+  }
+
+  test() {
+    // Метод test класса Car
+    this.#changeVin(); // Вызывает приватный метод #changeVin
+  }
+
+  static #field = 3; // Приватное статическое свойство #field
+
   static {
-    let b = 5;
-    this.a = 5;
+    this.#field = 5; // Инициализация приватного статического свойства #field в блоке static
   }
 }
-Test.hello();
-console.log(Test.a);
+
+const car = new Car(); // Создаем экземпляр класса Car
+
+car.test(); // Вызываем метод test экземпляра car
