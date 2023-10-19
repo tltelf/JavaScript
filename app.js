6261ad294d4a0ffd8e73a46ac17eebe10b612961
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-19 15:24:29 +0400

    Object.create

--- app.js
+++ app.js
@@ -1,96 +1,21 @@
 'use strict';
 
-/*
-	Реализовать класс пользователя, со свойствами
-	- логин
-	- пароль
-	Причём пароль при установке должен переворачиваться
-	и в таком виде храниться.
-	Пароль и логин после создания изменить нельзя. Так же у
-	класса добавить методы
-	- Смены пароля (передаём старый и новый пароль)
-	- Сверки пароля
-*/
+const User = {
+  init(email, password) {
+    (this.email = email), (this.password = password);
+  },
+  log() {
+    console.log('Log');
+  },
+};
 
-class User {
-  #login;
-  #_password;
-  constructor(login, password) {
-    this.#login = login;
-    this.#password = password;
-  }
+const user = Object.create(User);
+console.log(user);
+user.log(); // Log
+user.init('a@a.ru', '123');
+console.log(user); // {email: 'a@a.ru', password: '123'}
+console.log(user.__proto__ === User); // true
 
-  get login() {
-    return this.#login;
-  }
-
-  set #password(password) {
-    this.#_password = password.split('').reverse().join('');
-  }
-
-  get #password() {
-    return this.#_password.split('').reverse().join('');
-  }
-
-  checkPass(password) {
-    return this.#password === password;
-  }
-
-  changePass(oldPass, newPass) {
-    if (!this.checkPass(oldPass)) {
-      console.log('Вы ввели неверный пароль');
-      return;
-    }
-    console.log('Пароль успешно изменен!');
-    this.#_password = newPass.split('').reverse().join('');
-  }
-}
-
-const user = new User('abc@gmail.com', '12345');
-console.log(user); // User {#login: 'abc@gmail.com', #_password: '54321'}
-console.log(user.checkPass('12345')); // true
-user.changePass('12678', 'abcdef'); // Вы ввели неверный пароль
-user.changePass('12345', 'abcdef'); // Пароль успешно изменен!
-console.log(user); // User {#login: 'abc@gmail.com', #_password: 'fedcba'}
-
-class User2 {
-  #login;
-  #password;
-
-  constructor(login, password) {
-    this.#login = login;
-    this.#password = this.reversePassword(password);
-  }
-
-  reversePassword(password) {
-    return password.split('').reverse().join('');
-  }
-
-  changePassword(oldPassword, newPassword) {
-    if (this.verifyPassword(oldPassword)) {
-      this.#password = this.reversePassword(newPassword);
-      console.log('Пароль успешно изменен!');
-      return true; // Пароль успешно изменен
-    } else {
-      return false; // Не удалось изменить пароль
-    }
-  }
-
-  verifyPassword(passwordToCheck) {
-    return this.#password === this.reversePassword(passwordToCheck);
-  }
-
-  getLogin() {
-    return this.#login;
-  }
-
-  // Методы для получения логина и проверки пароля
-}
-
-const user2 = new User2('Логин', '123');
-console.log(user2.getLogin()); // Логин
-console.log(user2.verifyPassword('123')); // true
-console.log(user2.verifyPassword('456')); // false
-
-user2.changePassword('123', '789'); // Пароль успешно изменен!
-console.log(user2.verifyPassword('789')); // true
+const admin = Object.create(user);
+admin.init('B@B.ru', '56789');
+console.log(admin); // {email: 'B@B.ru', password: '56789'}
