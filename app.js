8ba45c11ed635ea5914e3d2f97bcec043fe1384b
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-22 16:56:42 +0400

    Получение данных

--- app.js
+++ app.js
@@ -1,48 +1,18 @@
 'use strict';
 
-/* Принцип инверсии зависимостей
-  (Dependency Inversion Principle - DIP):
-  зависимости внутри системы должны строиться относительно абстракций, а не конкретных классов.
-*/
+function req(id) {
+  const request = new XMLHttpRequest();
+  request.open('GET', 'https://dummyjson.com/products/' + id);
+  request.send();
 
-class DB {
-  save(items) {
-    console.log(`Saved: ${items}`);
-  }
+  request.addEventListener('load', function () {
+    const data = JSON.parse(this.responseText);
+    console.log(data);
+  });
 }
 
-class MongoDB extends DB {
-  save(items) {
-    console.log(`Saved to Mongo: ${items}`);
-  }
-}
+req(1);
+req('');
+req(3);
 
-// Здесь ToDoList не зависит от конкретной базы данных
-class ToDoList {
-  items = [1, 2, 3];
-  db;
-
-  constructor(db) {
-    this.db = db;
-  }
-
-  saveToDb() {
-    this.db.save(this.items);
-  }
-}
-
-const list = new ToDoList(new DB());
-list.saveToDb(); // Saved: 1,2,3
-const list2 = new ToDoList(new MongoDB());
-list2.saveToDb(); // Saved to Mongo: 1,2,3
-
-// Здесь ToDoList зависит от конкретной базы данных
-// и чтобы её поменять, придется менять код
-class ToDoList2 {
-  items = [1, 2, 3];
-  db = new DB();
-
-  saveToDb() {
-    this.db.save(this.items);
-  }
-}
+console.log('end');
