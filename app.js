b56021e8a794dd7ca55e2c3955c800d8162c8c74
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-22 22:10:04 +0400

    Цепочка promise

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 'use strict';
 
-fetch('https://dummyjson.com/products/1')
+fetch('https://dummyjson.com/products')
+  .then((response) => response.json())
+  .then(({ products }) => {
+    console.log(products);
+    return fetch('https://dummyjson.com/products/' + products[0].id);
+  })
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
