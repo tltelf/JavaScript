046b5c8f0ecffce59e678115d4c60cf6af9f43d3
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-23 01:22:36 +0400

    Упражнение - Select выбора категорий

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 'use strict';
 
-fetch('https://dummyjson.com/products')
-  .then((response) => response.json())
-  .then(({ products }) => {
-    console.log(products);
-    return fetch('https://dummyjson.com/products/' + products[0].id);
-  })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => console.log(error))
-  .finally(() => {
-    console.log('Finally');
+/*
+	Сделать запрос на https://dummyjson.com/products/categories,
+	получить список категорий и отобразить <select> выбора категорий.
+*/
+
+function createSelect(arrCategories) {
+  const filter = document.querySelector('.filter');
+  const select = document.createElement('select');
+  filter.appendChild(select);
+  arrCategories.map((category) => {
+    const option = document.createElement('option');
+    option.innerText = category;
+    select.appendChild(option);
   });
+}
+
+function getCategories() {
+  fetch('https://dummyjson.com/products/categories')
+    .then((response) => response.json())
+    .then((data) => createSelect(data))
+    .catch((error) => console.error(`Ошибка: ${error}`));
+}
+
+getCategories();
+
+/* Второй способ 
+
+function createSelect(array) {
+	const el = document.querySelector('.filter');
+	el.innerHTML = `<select>
+		${array.map(arrEl => `<option value=${arrEl}>${arrEl}</option>`)}
+	</select>`
+}
+
+function getCategories() {
+	fetch('https://dummyjson.com/products/categories')
+	.then(response => response.json())
+	.then(data => createSelect(data))
+	.catch(error => console.error(`Ошибка: ${error}`))
+}
+
+getCategories();
+
+*/
