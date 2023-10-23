768f756cdb82fde4cf0d12c5d5ab9739f20d4e02
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-23 12:46:11 +0400

    Упражнение - Функция запросов

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 'use strict';
 
+function getData(url, errorMessage) {
+  return fetch(url).then((res) => {
+    if (!res.ok) {
+      throw new Error(`${errorMessage} ${res.status}`);
+    }
+    return res.json();
+  });
+}
+
+/* СТАЛО */
+
+getData('https://dummyjson.com/products', 'Is error')
+  .then(({ products }) => {
+    console.log(products);
+    return getData('https://dummyjson.com/products/' + products[0].id);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    const filter = document.querySelector('.filter');
+    filter.innerHTML = error.message;
+  });
+
+/* БЫЛО */
+
 fetch('https://dummyjson.com/productss')
   .then((response) => {
     if (!response.ok) {
