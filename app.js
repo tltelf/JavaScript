a8ff88c5b6dbb9b5b97c8e644cbfd24a0007da58
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-28 13:58:52 +0400

    Try catch

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 'use strict';
 
 async function getProducts() {
-  // fetch('https://dummyjson.com/products')
-  //   .then((response) => response.json())
-  //   .then((data) => console.log(data));
-  const productsResponse = await fetch('https://dummyjson.com/products');
-  const { products } = await productsResponse.json();
-  console.log(products);
+  try {
+    const productsResponse = await fetch('https://dummyjson.com/products');
+    if (!productsResponse.ok) {
+      throw new Error(productsResponse.status);
+    }
+    const { products } = await productsResponse.json();
+    console.log(products);
 
-  const productResponse = await fetch(
-    'https://dummyjson.com/products/' + products[0].id
-  );
-  const product = await productResponse.json();
-  console.log(product);
+    const productResponse = await fetch(
+      'https://dummyjson.com/products/' + products[0].id
+    );
+    const product = await productResponse.json();
+    console.log(product);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    console.log('Finally');
+  }
+}
+
+try {
+  JSON.parse('{d');
+} catch (e) {
+  console.error('sdsf');
 }
 
 getProducts();
