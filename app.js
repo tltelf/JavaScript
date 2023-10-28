dca3f7eb591e4fb9616ac72eeb0c8d7f5f05772e
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-28 22:25:54 +0400

    Последовательность выполнения

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 'use strict';
 
-class ProductRepository {
-  async getProducts() {
-    const response = await fetch('https://dummyjson.com/products');
-    console.log(await response.json());
-  }
-}
-
-const repo = new ProductRepository();
-repo.getProducts();
-
 const asyncArrow = async () => {
-  const response = await fetch('https://dummyjson.com/products');
-  console.log(await response.json());
+  try {
+    const response = await fetch('https://dummyjson.com/products');
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
 };
 
-asyncArrow();
+// console.log('1');
+// asyncArrow()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((e) => console.log(e))
+//   .finally(() => console.log('2'));
+
+(async () => {
+  console.log(1);
+  const res = await asyncArrow();
+  console.log(res);
+  console.log('2');
+})();
