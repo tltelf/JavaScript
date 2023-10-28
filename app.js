91a77c05ecbfa3ad96d8551a10292e86c4ad0e98
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-28 13:42:57 +0400

    Async await

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
 'use strict';
 
-function myFetch(url) {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.send();
+async function getProducts() {
+  // fetch('https://dummyjson.com/products')
+  //   .then((response) => response.json())
+  //   .then((data) => console.log(data));
+  const productsResponse = await fetch('https://dummyjson.com/products');
+  const { products } = await productsResponse.json();
+  console.log(products);
 
-    request.addEventListener('load', function () {
-      if (this.status > 400) {
-        reject(new Error(this.status));
-      }
-      resolve(this.responseText);
-    });
-
-    request.addEventListener('error', function () {
-      reject(new Error(this.status));
-    });
-
-    request.addEventListener('timeout', function () {
-      reject(new Error('Timeout'));
-    });
-  });
+  const productResponse = await fetch(
+    'https://dummyjson.com/products/' + products[0].id
+  );
+  const product = await productResponse.json();
+  console.log(product);
 }
 
-myFetch('https://dummyjson.com/products')
-  .then((data) => console.log(JSON.parse(data)))
-  .catch((err) => console.error(err));
+getProducts();
+console.log('End');
