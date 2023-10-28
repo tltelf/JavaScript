122eb1ce12617608c1e6dcbdb455c832b20f2b48
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-28 21:56:25 +0400

    Асинхронные методы

--- app.js
+++ app.js
@@ -1,64 +1,18 @@
 'use strict';
 
-/*
-	Получить геолокацию пользователя через
-	Geolocation.getCurrentPosition() (WEB API)
-	и по координатам определить город, отправив запрос:
-	https://api.bigdatacloud.net/data/reverse-geocode-client?
-	latitude=00&longitude=00
-*/
-
-function getMyCoordinates() {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(
-      ({ coords }) => {
-        console.log(coords);
-        resolve({
-          latitude: coords.latitude,
-          longitude: coords.longitude,
-        });
-      },
-      (err) => {
-        reject(err);
-      }
-    );
-  });
-}
-
-async function getMyCity() {
-  try {
-    const { latitude, longitude } = await getMyCoordinates();
-    const dataResponse = await fetch(
-      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
-    );
-    if (!dataResponse.ok) {
-      throw new Error(dataResponse.status);
-    }
-    const { city } = await dataResponse.json();
-    console.log(city);
-  } catch (e) {
-    console.log(e);
+class ProductRepository {
+  async getProducts() {
+    const response = await fetch('https://dummyjson.com/products');
+    console.log(await response.json());
   }
 }
 
-getMyCity();
+const repo = new ProductRepository();
+repo.getProducts();
 
-async function getPosition() {
-  let latitude;
-  let longitude;
+const asyncArrow = async () => {
+  const response = await fetch('https://dummyjson.com/products');
+  console.log(await response.json());
+};
 
-  navigator.geolocation.getCurrentPosition(({ coords }) => {
-    console.log(coords);
-    latitude = coords.latitude;
-    longitude = coords.longitude;
-  });
-
-  const dataResponse = await fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
-  );
-  const { city } = await dataResponse.json();
-
-  return city;
-}
-
-getPosition().then((data) => console.log(data));
+asyncArrow();
