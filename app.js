5c1b42c799eb052e2ad70760c1d6936a465d02cc
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-29 12:25:30 +0400

    Другие комбинации Promise

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 'use strict';
 
-async function getAllProducts() {
-  const response = await fetch('https://dummyjson.com/products');
-  return response.json();
-}
-
 async function getProduct(id) {
   const response = await fetch('https://dummyjson.com/products/' + id);
   return response.json();
 }
 
+async function getProductError(id) {
+  const response = await fetch('https://dummyjsons.com/products/' + id);
+  return response.json();
+}
+
 async function main() {
-  const { products } = await getAllProducts();
-  // const res = await Promise.all([getProduct(1), getProduct(2), getProduct(3)]);
-  const res = await Promise.all(
-    products.map((product) => getProduct(product.id))
-  );
-  console.log(res);
-  // for (const product of products) {
-  //   const res = await getProduct(product.id)
-  //   console.log(res);
-  // }
+  const res1 = await Promise.all([getProduct(1), getProduct(2)]);
+  console.log(res1);
+
+  const res2 = await Promise.allSettled([getProduct(1), getProduct(2)]);
+  console.log(res2);
+
+  const res3 = await Promise.race([getProduct(1), getProduct(2)]);
+  console.log(res3);
 }
 
 main();
