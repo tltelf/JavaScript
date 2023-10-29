354043b79e186960a5ec83d2071466f41433162c
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-29 17:55:10 +0400

    Всплытие событий

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 'use strict';
 
 const button = document.querySelector('.button');
+const inner = document.querySelector('.inner');
+const wrapper = document.querySelector('.wrapper');
 
-const eventHandler = function (event) {
-  console.log('Event 1');
-};
-
-button.addEventListener('click', eventHandler);
-button.addEventListener('click', (event) => {
-  console.log('Event 2');
-  button.removeEventListener('click', eventHandler);
+button.addEventListener('click', function (event) {
+  console.log('button');
+  console.log(event.target);
+  console.log(event.currentTarget);
+  this.style.backgroundColor = 'purple';
 });
+
+inner.addEventListener('click', function (event) {
+  console.log('inner');
+  console.log(event.target);
+  console.log(event.currentTarget);
+  this.style.backgroundColor = 'grey';
+  // event.stopPropagation();
+});
+
+wrapper.addEventListener('click', function (event) {
+  console.log('wrapper');
+  console.log(event.target);
+  console.log(event.currentTarget);
+  this.style.backgroundColor = 'antiquewhite';
+});
+
+wrapper.addEventListener(
+  'click',
+  function (event) {
+    console.log('wrapper');
+    console.log(event.target);
+    console.log(event.currentTarget);
+    this.style.backgroundColor = 'antiquewhite';
+  },
+  true
+);
