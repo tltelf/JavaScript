6a14038cc0596052f705f8168a48f12f1c36131c
Kirill Lyashenko <tltelf@yandex.ru>
2023-10-31 13:49:17 +0400

    Await на верхнем уровне

--- calc.js
+++ calc.js
@@ -9,3 +9,10 @@ export function sub(f, s) {
 }
 
 console.log('d');
+
+async function getProducts() {
+  const res = await fetch('https://dummyjson.com/products');
+  return res.json();
+}
+
+export const res = await getProducts();
