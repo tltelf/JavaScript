0a55af49af14fad198e2eaf23075d965d5cad5a8
Kirill Lyashenko <tltelf@yandex.ru>
2023-11-03 09:26:44 +0400

    State и реактивности

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ import { MainView } from './views/main/main.js';
 
 class App {
   routes = [{ path: '', view: MainView }];
+
+  appState = {
+    favourites: [],
+    test: [],
+  };
+
   constructor() {
     window.addEventListener('hashchange', this.route.bind(this));
     this.route();
@@ -12,7 +18,7 @@ class App {
       this.currentView.destroy();
     }
     const view = this.routes.find((r) => r.path == location.hash).view;
-    this.currentView = new view();
+    this.currentView = new view(this.appState);
     this.currentView.render();
   }
 }
