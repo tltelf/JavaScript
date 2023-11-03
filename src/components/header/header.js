b654414a66fba307a08f1b9ede6985502e5c0028
Kirill Lyashenko <tltelf@yandex.ru>
2023-11-03 12:54:30 +0400

    Вёрстка шапки

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import { DivComponent } from '../../common/div-component.js';
+import './header.css';
 
 export class Header extends DivComponent {
   constructor(appState) {
@@ -9,7 +10,24 @@ export class Header extends DivComponent {
   render() {
     this.el.innerHTML = '';
     this.el.classList.add('header');
-    this.el.innerHTML = `<div><img src="./static/logo.svg" alt="Логотип"</div>`;
+    this.el.innerHTML = `
+        <div>
+            <img src="./static/logo.svg" alt="Логотип" />
+        </div>
+        <div class="menu">
+            <a class="menu__item" href="#">
+                <img src="./static/search.svg" alt="Иконка поиска" />
+                Поиск книг
+            </a>
+            <a class="menu__item" href="#favourites">
+                <img src="./static/favourites.svg" alt="Иконка избранного" />
+                Избранное
+                <div class="menu__counter">
+                    ${this.appState.favourites.length}
+                </div>
+            </a>
+        </div>
+    `;
     return this.el;
   }
 }
